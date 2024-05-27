7b8fa093e929ecc1c9abe114dee9dafe7f31210f
Nihal1203 <nihalcharola11@gmail.com>
2024-05-27 17:37:01 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // require('dotenv').config({path:'./env'})
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-// import express from "express";
 
+import express from "express";
+const app=express()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -11,4 +12,12 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on port:${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection Failed !!!",err)
+})
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler=(requestHandler)=>{
+   (req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next))
+    .catch((err)=>next(err))
+   } 
+}
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//   try {
+//     await fn(req,res,next)
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       succes:false,
+//       message:err.message
+//     })
+//   }
+// }
+
+export {asyncHandler}
